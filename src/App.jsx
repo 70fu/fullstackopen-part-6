c8b4f26ce94bfb77641c0f25c27b593ccb6cedf3
Simon Reiser <ein.simonreiser@gmail.com>
2023-10-01 16:18:06 +0900

    Save and load anecdotes from json-server backend (6.14-6.18)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { voteFor } from './reducers/anecdoteReducer'
+import { useEffect } from 'react'
+import { initAnecdotes, voteFor } from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(initAnecdotes())
+  }, [dispatch]);
+
   return (
     <div>
       <Notification/>
--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -1,29 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit'
-
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
-const getId = () => Number((100000 * Math.random()).toFixed(0))
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
+import anecdoteService from '../services/anecdotes';
 
 const anecdoteSlice = createSlice({
   name:'anecdotes',
-  initialState:initialState,
+  initialState:[],
   reducers:{
     voteFor(state,action){
       console.log('voting for',action.payload);
@@ -34,12 +14,30 @@ const anecdoteSlice = createSlice({
         console.error('could not vote for anecdote with id',action.payload);
       }
     },
-    createAnecdote(state,action){
-      const newAnecdote = asObject(action.payload);
-      state.push(newAnecdote);
+    appendAnecdote(state, action){
+      state.push(action.payload);
+    },
+    setAnecdotes(state, action){
+      return action.payload;
     }
   }
 });
 
-export const { voteFor, createAnecdote } = anecdoteSlice.actions;
+
+export const { voteFor, appendAnecdote, setAnecdotes } = anecdoteSlice.actions;
+
+export const initAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll();
+    dispatch(setAnecdotes(anecdotes));
+  }
+}
+
+export const createAnecdote = (content) => {
+  return async (dispatch) => {
+    const anecdote = await anecdoteService.createNew(content);
+    dispatch(appendAnecdote(anecdote));
+  }
+}
+
 export default anecdoteSlice.reducer;
