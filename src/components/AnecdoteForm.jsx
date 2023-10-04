35dab683aa6c0aedca6a06403ea5f25ab6f381f8
Simon Reiser <ein.simonreiser@gmail.com>
2023-10-04 10:15:43 +0900

    Add creating new anecdotes (6.21)

--- src/components/AnecdoteForm.jsx
+++ src/components/AnecdoteForm.jsx
@@ -1,11 +1,23 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createAnecdote } from '../requests'
+
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient();
+
+  const createAnecdoteMutation = useMutation({
+    mutationFn:createAnecdote,
+    onSuccess: (newAnecdote) => {
+      queryClient.setQueryData(['anecdotes'], (oldData) => oldData.concat(newAnecdote))
+    }
+  });
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+    createAnecdoteMutation.mutate({ content, votes:0 });
+  }
 
   return (
     <div>
--- src/requests.js
+++ src/requests.js
@@ -1,3 +1,7 @@
 import axios from 'axios'
 
-export const getAnecdotes = () => axios.get('http://localhost:3001/anecdotes').then(res => res.data);
+const BASE_URL = 'http://localhost:3001/anecdotes';
+
+export const getAnecdotes = () => axios.get(BASE_URL).then(res => res.data);
+
+export const createAnecdote = (newAnecdote) => axios.post(BASE_URL,newAnecdote).then(res => res.data);
